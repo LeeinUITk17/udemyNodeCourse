4e0006fd4799476a43c044d438c0c547499a74c5
Lê Thanh Tài <119780047+LeeinUITk17@users.noreply.github.com>
2023-11-05 23:22:01 +0700

    fix

--- app.js
+++
@@ -1,9 +0,0 @@
-console.log('hello world');
-function sum(a,b){
-    return a+b;
-}
-//
-const valuesum=sum(5,4);
-console.log(`value of sum is ${valuesum}`);
-console.log("Total: ",valuesum);
-console.log("PROCESS: ",process);
