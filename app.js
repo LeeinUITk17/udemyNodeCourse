9def201200380d04950550952e78ad53edaaa7bc
Lê Thanh Tài <119780047+LeeinUITk17@users.noreply.github.com>
2023-11-05 23:20:33 +0700

    new course

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log('hello world');
+function sum(a,b){
+    return a+b;
+}
+//
+const valuesum=sum(5,4);
+console.log(`value of sum is ${valuesum}`);
+console.log("Total: ",valuesum);
+console.log("PROCESS: ",process);
