8a229d24510127ea1b5e694d2ef9fa0ca5903628
Lê Thanh Tài <mrtaivietbac@gmail.com>
2023-11-06 09:45:16 +0700

    update lesion02

--- app.js
+++ app.js
@@ -1,20 +1,56 @@
-//console.log("PROCESS:",process);
- const helper=require('./helper');
-// const total=helper.sum(3231,3123);
-// console.log("Total:",total);
+const helper = require('./helper');
+const readline = require('readline');
+const { sum, countEvenNumbers } = require("./helper.js"); // Import the specific functions you need
 
-const readline=require('readline')
-const rl=readline.createInterface({
+const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-function handleUserInput(){
-    rl.question('enter numbers seperated by spaces: ',input=>{
-        const numbers=input.split(' ').map(Number);
-        const evennumber=helper.countEvenNumbers(numbers);
-        console.log('the number of even number is: '+evennumber);
+function handleUserInput() {
+    rl.question('Enter numbers separated by spaces: ', input => {
+        const numbers = input.split(' ').map(Number);
+        const evenNumber = countEvenNumbers(numbers); // Use the imported function
+        console.log('The number of even numbers is: ' + evenNumber);
         rl.close();
     });
 }
-handleUserInput();
+
+handleUserInput(); // Call the function
+
+const total = sum(2121, 21412);
+console.log("Total:", total);
+ const http=require('http');
+ const server=http.createServer((req,res)=>{
+res.end('hello world  from nodejs');
+ });
+ server.listen(3000);
+
+const express=require('express');
+const app=express();
+app.get("/",(req,res)=>{
+    res.send('hey whats up from express');
+})
+app.listen(4000);
+
+const fs = require('fs');
+const { error } = require('console');
+const filename = "target.txt";
+
+fs.watch(filename, () => console.log("File changed"));
+
+fs.readFile(filename, (err, data) => {
+  if (err) {
+    console.error(err); // Corrected to log the error
+  } else {
+    console.log(data.toString()); // Convert the data to a string and then log it
+  }
+});
+console.log('nodejs async programing..?');
+
+const data=fs.readFileSync(filename);
+console.log(data.toString());
+
+
+
+
--- helper.js
+++ helper.js
@@ -1,16 +1,15 @@
-function sum(value1, value2) {
-    return value1 + value2;
-}
-
 function countEvenNumbers(array) {
     const evenNumbers = array.filter(num => num % 2 === 0);
     return evenNumbers.length;
 }
 
+function sum(value1, value2) {
+    return value1 + value2;
+}
+
 module.exports = {
+    countEvenNumbers,
     sum
 };
-module.exports={
-    countEvenNumbers
-};
-console.log("PROCESS: ",process);
+
+console.log("PROCESS: ", process);
