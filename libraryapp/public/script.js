f2d7fbbb954ec82bad15a90e14d58e7300e48796
Lê Thanh Tài <mrtaivietbac@gmail.com>
2023-11-17 16:24:35 +0700

    update app x2

--- libraryapp/public/script.js
+++ libraryapp/public/script.js
@@ -1,19 +1,41 @@
 async function searchBooks() {
     const searchInput = document.getElementById('searchInput').value;
+    const bookList=document.getElementById('bookList');
+    const loadingSpinner=document.getElementById('loadingSpinner');
 
     try {
-        const response = await fetch(`/search?title=${searchInput}`);
-        const data = await response.json();
+        // const response = await fetch(`/search?title=${searchInput}`);
+        // const data = await response.json();
 
-        const bookList = document.getElementById('bookList');
-        bookList.innerHTML = '';
+        // const bookList = document.getElementById('bookList');
+        // bookList.innerHTML = '';
 
-        data.forEach(book => {
-            const listItem = document.createElement('li');
-            listItem.textContent = `${book.title} by ${book.author}`;
-            bookList.appendChild(listItem);
-        });
+        // data.forEach(book => {
+        //     const listItem = document.createElement('li');
+        //     listItem.textContent = `${book.title} by ${book.author}`;
+        //     bookList.appendChild(listItem);
+       // });
+
+     loadingSpinner.classList.remove('hidden');
+     const response=await fetch(`/search?title=${searchInput}`);
+     const data=await response.json();
+
+     bookList.innerHTML='';
+
+     data.forEach(book => {
+        const listItem=document.createElement('li');
+        listItem.textContent=`${book.title} by ${book.author}`;
+        bookList.appendChild(listItem);
+     });
+
     } catch (error) {
         console.error('Error fetching book data:', error);
+    } finally{
+        loadingSpinner.classList.add('hidden');
     }
+}
+
+function clearResults(){
+    document.getElementById('bookList').innerHTML='';
+    document.getElementById('searchInput').value='';
 }
