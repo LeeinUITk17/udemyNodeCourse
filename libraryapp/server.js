786c7d5e76ffb3f8f2caa87b2354318ac0395dbc
Lê Thanh Tài <119780047+LeeinUITk17@users.noreply.github.com>
2023-11-17 15:53:19 +0700

    update inf

--- libraryapp/server.js
+++ libraryapp/server.js
@@ -5,6 +5,17 @@ const PORT = 3000;
 const library = [
     { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' },
     { title: 'To Kill a Mockingbird', author: 'Harper Lee' },
+    { title: '1984', author: 'George Orwell' },
+    { title: 'Pride and Prejudice', author: 'Jane Austen' },
+    { title: 'The Catcher in the Rye', author: 'J.D. Salinger' },
+    { title: 'The Hobbit', author: 'J.R.R. Tolkien' },
+    { title: 'The Lord of the Rings', author: 'J.R.R. Tolkien' },
+    { title: 'Harry Potter and the Sorcerer\'s Stone', author: 'J.K. Rowling' },
+    { title: 'To the Lighthouse', author: 'Virginia Woolf' },
+    { title: 'One Hundred Years of Solitude', author: 'Gabriel García Márquez' },
+    { title: 'The Hitchhiker\'s Guide to the Galaxy', author: 'Douglas Adams' },
+    { title: 'The Shining', author: 'Stephen King' },
+    { title: 'The Girl with the Dragon Tattoo', author: 'Stieg Larsson' },
     // Add more books as needed
 ];
 
