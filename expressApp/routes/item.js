48cfbb7daf06b447d1650d3b84254f9e8593041e
Lê Thanh Tài <119780047+LeeinUITk17@users.noreply.github.com>
2023-11-27 07:19:50 +0700

    update

---
+++ expressApp/routes/item.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/list', function(req, res, next) {
+  //res.send('List Videos demo');
+  res.render('itemList',{title:'List Items'})
+});
+
+// router.get('/video/add', function(req, res, next) {
+//     res.send('add Videos demo');
+//   });
+
+module.exports = router;
