f719a5a6cee9eb1084796e7f7b4fcddbc87cf5d3
Lê Thanh Tài <119780047+LeeinUITk17@users.noreply.github.com>
2023-12-11 23:06:25 +0700

    update db connected with html file

--- expressApp/routes/index.js
+++ expressApp/routes/index.js
@@ -8,24 +8,28 @@ router.get('/', function(req, res, next) {
 router.get('/dashboard', function(req, res, next) {
   res.render('dashboard');
 });
-router.get('/list',(req,res,next)=>{
+router.get('/list', (req, res, next) => {
   ItemsModel.find({})
-  .then((items) => {
-    if (items && items.length > 0) {
-      console.log('Items found:');
-      items.forEach((item, index) => {
-        console.log(`Item ${index + 1}:`, item);
-      });
-    } else {
-      console.log('No items found.');
-    }
-  })
-  .catch((err) => {
-    console.error('Error fetching items:', err);
-    // Handle the error appropriately
-  });
-  res.render('list');
-})
+    .then((items) => {
+      if (items && items.length > 0) {
+        console.log('Items found:');
+        items.forEach((item, index) => {
+          console.log(`Item ${index + 1}:`, item);
+        });
+        res.render('list', { items }); // Fix: Enclose res.render with curly braces
+      } else {
+        console.log('No items found.');
+        res.render('list', { items: [] }); // Assuming you want to render with an empty array when no items are found
+      }
+    })
+    .catch((err) => {
+      console.error('Error fetching items:', err);
+      // Handle the error appropriately
+      res.status(500).send('Internal Server Error'); // Example: Send a 500 status in case of an error
+    });
+});
+
+
 router.get('/form',function(req,res,next){
   res.render('form');
 })
